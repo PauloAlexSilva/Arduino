58f06f7394823bdabdec592d9830a810dcf36949
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-03-07 21:13:23 +0100

    Update Test.ino

--- Test/Test.ino
+++ Test/Test.ino
@@ -1,9 +1,24 @@
+//Import Bibliotecas
+
+#include <LiquidCrystal_I2C.h>
+
+//Definições
+
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 bool leituraSensor;
 bool leituraAnterior;
 
+int humidade;
+int totalRegas;
+
 void setup() {
 
+  lcd.init();  // Inicia a comunicação com o display
+  lcd.backlight();  // Liga a luz do display
+  lcd.print("Bem Vindo!");
+  delay(3000);
+  
   Serial.begin(9600); //Enviar e receber dados em 9600 baud
   
   //Sensor
@@ -21,37 +36,70 @@ void setup() {
 
 void loop() {
 
+  // Converte valor sensor em %
+  humidade = analogRead(leituraSensor);
+  humidade = map(humidade, 1023, 0, 0, 100);
   
-  
+  lcd.clear();  // Limpa o display
+  lcd.print("Humidade do Solo");
+  lcd.setCursor(0,1);  // Posiciona o cursor na primeira coluna da segunda linha
+  lcd.print(humidade);
+  lcd.print("%");
+  delay(2000);
+
+  Serial.println("Valor Sensor: ");
   Serial.println(analogRead(leituraSensor));
+  Serial.println("Valor Convertido: ");
+  Serial.println(humidade);
 
-  if (analogRead(leituraSensor) >  700) {
+  //Solo seco
+  if (humidade < 50) {
 
-    //Solo seco
-    digitalWrite(4, HIGH);  //Vermelho
+    digitalWrite(5, HIGH);  //Vermelho
     digitalWrite(7, LOW);  //Verde
 
-    delay(1000);
+    lcd.clear();  // Limpa o display
+    lcd.print("Solo Seco!");
+    lcd.setCursor(0,1);  // Posiciona o cursor na primeira coluna da segunda linha
+    lcd.print("A ligar a rega.");
+    delay(2000);
     
-    while (analogRead(leituraSensor) > 700) {
-
-      digitalWrite(4, LOW);  //Vermelho
+    while (humidade < 50) {
+      
+      totalRegas ++;
+      
+      lcd.clear();
+      lcd.print("Rega ligada");
+      lcd.setCursor(0,1);  
+      lcd.print("Humidade: ");
+      lcd.print(humidade);
+      lcd.print("%");
+      
+      //digitalWrite(5, LOW);  //Vermelho
       digitalWrite(6, HIGH);  //Amarelo
     
       digitalWrite(12, HIGH);  //Rele
       delay(5000);
       digitalWrite(12, LOW); //Rele
-
       delay(1000);
+
+      // Converte valor sensor em %
+      humidade = analogRead(leituraSensor);
+      humidade = map(humidade, 1023, 0, 0, 100);
     }
     digitalWrite(6, LOW);  //Amarelo
 
   } else {
 
     // solo umido
-    digitalWrite(4, LOW);  //Vermelho
+    lcd.clear();  // Limpa o display
+    lcd.print("Solo Humido");
+    lcd.setCursor(0,1);  // Posiciona o cursor na primeira coluna da segunda linha
+    lcd.print("Total Regas:");
+    lcd.print(totalRegas);
+    delay(2000);
+    
+    digitalWrite(5, LOW);  //Vermelho
     digitalWrite(7, HIGH); //Verde
-
   }
-
 }
